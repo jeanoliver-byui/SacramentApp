1a2a9ef7fd9e12051e2c2d1ef08c08a045e33fd6
Jean Oliveira <oli15014@byui.edu>
2017-12-17 20:17:04 -0700

    Add project files.

---
+++ SacramentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SacramentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
